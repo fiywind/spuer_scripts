b34afbe798fcad91d748421985856e863e3b0b2b
fiywind <justlcd@163.com>
2026-04-07 21:16:59 +0800

    Update caiyun_svip.js

--- caiyun_svip.js
+++ caiyun_svip.js
@@ -2,18 +2,19 @@
 彩云天气 SVIP 本地解锁脚本
 */
 let obj = JSON.parse($response.body);
+
 if ($request.url.includes("/v1/config/membership/svip/rights")) {
-    obj.data = {
-      "is_svip": true,
-      "svip_expired_at": 4070880000,
-      "is_vip": true,
-      "vip_expired_at": 4070880000
-    };
+    obj.data = obj.data || {};
+    obj.data.is_svip = true;
+    obj.data.svip_expired_at = 4070880000;
+    obj.data.is_vip = true;
+    obj.data.vip_expired_at = 4070880000;
 }
+
 if ($request.url.includes("/v3/config/membership/svip/rights")) {
-    obj.result = {
-      "is_svip": true,
-      "expires_at": 4070880000
-    };
+    obj.result = obj.result || {};
+    obj.result.is_svip = true;
+    obj.result.expires_at = 4070880000;
 }
+
 $done({body: JSON.stringify(obj)});
