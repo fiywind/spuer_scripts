d20f4e7a7060edd84d6844865dcc6c052e3dad51
fiywind <justlcd@163.com>
2026-04-07 14:24:27 +0800

    Update aqc.js

--- aqc.js
+++ aqc.js
@@ -1,33 +1,32 @@
 /**
- * @name 爱企查超级会员/专业会员解锁 (增强版)
+ * @name 爱企查超级会员/专业会员解锁终极版
  */
 
 let body = $response.body;
 if (!body) $done({});
 let obj = JSON.parse(body);
-const url = $request.url;
 
-// 统一解锁函数
-const fillVip = (data) => {
-    if (data) {
-        data.isVip = "1";
-        data.is_vip = 1;
-        data.vipType = "2";
-        data.vip_type = 2;
-        data.vipEndTime = "2099-12-31";
-        data.vip_end_time = "2099-12-31";
-        data.isExpert = "1";
-        data.is_expert = 1;
-        data.consumeVip = "1";
+// 核心解锁逻辑
+const unlock = (item) => {
+    if (item && typeof item === 'object') {
+        item.isVip = "1";
+        item.is_vip = 1;
+        item.vipType = "2";
+        item.vip_type = 2;
+        item.vipEndTime = "2099-12-31";
+        item.vip_end_time = "2099-12-31";
+        item.isExpert = "1";
+        item.is_expert = 1;
+        item.consumeVip = "1";
     }
 };
 
-// 匹配所有包含用户信息、VIP信息的接口
-if (url.includes("ajax")) {
-    fillVip(obj.data);
-    // 针对某些接口 data 就是数组的情况
+// 递归遍历所有名为 data 的层级
+if (obj.data) {
     if (Array.isArray(obj.data)) {
-        obj.data.forEach(item => fillVip(item));
+        obj.data.forEach(i => unlock(i));
+    } else {
+        unlock(obj.data);
     }
 }
 
