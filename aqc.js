c22ebaf098482ec2eff6221621b7ece6ae5ee54d
fiywind <justlcd@163.com>
2026-04-07 14:32:30 +0800

    Update aqc.js

--- aqc.js
+++ aqc.js
@@ -1,14 +1,14 @@
 /**
- * @name 爱企查超级会员/专业会员解锁终极版
+ * @name 爱企查超级会员/专业会员解锁 (全字段加强版)
  */
 
 let body = $response.body;
 if (!body) $done({});
 let obj = JSON.parse(body);
 
-// 核心解锁逻辑
 const unlock = (item) => {
     if (item && typeof item === 'object') {
+        // 覆盖所有可能的 VIP 相关字段
         item.isVip = "1";
         item.is_vip = 1;
         item.vipType = "2";
@@ -18,10 +18,13 @@ const unlock = (item) => {
         item.isExpert = "1";
         item.is_expert = 1;
         item.consumeVip = "1";
+        item.vip_status = 1;
+        item.expert_status = 1;
+        item.expertEndTime = "2099-12-31";
     }
 };
 
-// 递归遍历所有名为 data 的层级
+// 递归处理 data 对象
 if (obj.data) {
     if (Array.isArray(obj.data)) {
         obj.data.forEach(i => unlock(i));
