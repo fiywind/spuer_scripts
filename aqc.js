c7b3a3bb819f264821dfabe4241ef21f853bbb7f
fiywind <justlcd@163.com>
2026-04-07 14:36:07 +0800

    Update aqc.js

--- aqc.js
+++ aqc.js
@@ -1,36 +1,34 @@
 /**
- * @name 爱企查超级会员/专业会员解锁 (全字段加强版)
+ * @name 爱企查超级会员/专业会员解锁 (暴力覆盖版)
  */
-
 let body = $response.body;
 if (!body) $done({});
 let obj = JSON.parse(body);
 
-const unlock = (item) => {
-    if (item && typeof item === 'object') {
-        // 覆盖所有可能的 VIP 相关字段
-        item.isVip = "1";
-        item.is_vip = 1;
-        item.vipType = "2";
-        item.vip_type = 2;
-        item.vipEndTime = "2099-12-31";
-        item.vip_end_time = "2099-12-31";
-        item.isExpert = "1";
-        item.is_expert = 1;
-        item.consumeVip = "1";
-        item.vip_status = 1;
-        item.expert_status = 1;
-        item.expertEndTime = "2099-12-31";
+const unlock = (v) => {
+    if (v && typeof v === 'object') {
+        // 字符串形式
+        v.isVip = "1";
+        v.vipType = "2";
+        v.vipEndTime = "2099-12-31";
+        // 数字形式
+        v.is_vip = 1;
+        v.vip_type = 2;
+        v.vip_end_time = 4070880000; // 2099年时间戳
+        v.vip_status = 1;
+        // 专家/权益字段
+        v.isExpert = "1";
+        v.consumeVip = "1";
+        v.expert_status = 1;
     }
 };
 
-// 递归处理 data 对象
+// 针对爱企查特定的多级数据结构进行递归处理
 if (obj.data) {
-    if (Array.isArray(obj.data)) {
-        obj.data.forEach(i => unlock(i));
-    } else {
-        unlock(obj.data);
-    }
+    unlock(obj.data);
+    if (obj.data.vip_info) unlock(obj.data.vip_info);
+    if (obj.data.user_info) unlock(obj.data.user_info);
+    if (Array.isArray(obj.data)) obj.data.forEach(item => unlock(item));
 }
 
 $done({ body: JSON.stringify(obj) });
